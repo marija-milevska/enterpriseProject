ceda7497fccf38bf4d1e3b89bdf7af3148c1e480
Marija Milevska <marija.milevska05@gmail.com>
2024-07-09 14:11:37 +0200

    feat: add fetching json data

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { columns } from "./TableModel.tsx";
 import "./Table.css";
 
-const BooksTable: React.FC = () => (
-  <Table columns={columns} className="table" />
-);
+interface Book {
+  id: number;
+  author: string;
+  title: string;
+  genre: string;
+}
+
+const BooksTable: React.FC = () => {
+  const [data, setData] = useState<Book[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://raw.githubusercontent.com/marija-milevska/enterpriseProject/main/public/books.json"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <Table columns={columns} className="table" dataSource={data} />;
+};
 
 export default BooksTable;
